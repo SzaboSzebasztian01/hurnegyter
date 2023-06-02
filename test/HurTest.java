b8be5f8acee443a6aff39152debf670d98618ab9
Szabó Szebasztián <szaboszebasztianlevente@ktch.hu>
2023-06-02 14:42:21 +0200

    slf4j

--- test/HurTest.java
+++ test/HurTest.java
@@ -1,3 +1,28 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class HurTest {
 
+    @Test
+    public void testCalcArea(){
+
+        Hur hur = new Hur();
+        double area = hur.calcArea(30, 35, 40, 45);
+        double expected = 1374.772708486752;
+
+        Assert.assertEquals(area, expected);
+
+    }
+    
+    @Test
+    public void testCalcArea2(){
+
+        Hur hur = new Hur();
+        double area = hur.calcArea(130, 135, 140, 145);
+        double expected = 18874.98344370135;
+
+        Assert.assertEquals(area, expected);
+
+    }
+    
 }
