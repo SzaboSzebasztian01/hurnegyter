f2effcc2deb9944a57020ae6e9a5ffa7a18d6f79
Szabó Szebasztián <szaboszebasztianlevente@ktch.hu>
2023-06-02 14:55:43 +0200

    test finished

--- test/HurTest.java
+++ test/HurTest.java
@@ -1,12 +1,21 @@
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class HurTest {
 
-    @Test
-    public void testCalcArea(){
+    Hur hur = new Hur();
 
-        Hur hur = new Hur();
+    @BeforeTest
+    public void testInit(){
+
+        this.hur = new Hur();
+
+    }
+
+    @Test
+    public void testCalcArea1(){
+
         double area = hur.calcArea(30, 35, 40, 45);
         double expected = 1374.772708486752;
 
@@ -17,9 +26,48 @@ public class HurTest {
     @Test
     public void testCalcArea2(){
 
-        Hur hur = new Hur();
         double area = hur.calcArea(130, 135, 140, 145);
         double expected = 18874.98344370135;
+
+        Assert.assertEquals(area, expected);
+
+    }
+    
+    @Test
+    public void testCalcArea3(){
+
+        double area = hur.calcArea(-130, 135, 140, 145);
+        double expected = -1;
+
+        Assert.assertEquals(area, expected);
+
+    }
+    
+    @Test
+    public void testCalcArea4(){
+
+        double area = hur.calcArea(130, -135, 140, 145);
+        double expected = -1;
+
+        Assert.assertEquals(area, expected);
+
+    }
+    
+    @Test
+    public void testCalcArea5(){
+
+        double area = hur.calcArea(130, 135, -140, 145);
+        double expected = -1;
+
+        Assert.assertEquals(area, expected);
+
+    }
+    
+    @Test
+    public void testCalcArea6(){
+
+        double area = hur.calcArea(130, 135, 140, -145);
+        double expected = -1;
 
         Assert.assertEquals(area, expected);
 
